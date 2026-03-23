b493b0c434f6d6e29174018624ffe67844091c56
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-23 12:01:23 +0000

    session-1/task2

--- session-1/task2/main.c
+++ session-1/task2/main.c
@@ -17,8 +17,15 @@ int main(void)
     push(stack,1);
     push(stack,2);
     traverseI(stack->top);  // display the stack
-	Node* node=pop(stack);
-	printf(" %i",node->data->value);
+	Node* node=pop(stack); //pop 2
+	printf("two: %i\n",node->data->value);
+	freeNode(pop(stack)); //pop 1 and free it since we won't use it
+	/*Node* nothing=pop(stack); //nothing on stack, testing
+	printf("nothing: %s\n",nothing==NULL?"NULL":"not NULL");
+	if(nothing!=NULL)
+	{
+		freeNode(nothing);
+	}*/
 
 	freeNode(node);
 	freeStack(stack);
