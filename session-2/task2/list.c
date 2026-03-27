b18bda3d03c339134e900fa9b320fb66c737863a
NWDQ0199 <nwdq0199@leeds.ac.uk>
2026-03-27 09:43:03 +0000

    session2-task2

--- session-2/task2/list.c
+++ session-2/task2/list.c
@@ -8,49 +8,59 @@
 /*
  * create a Data item for the List
  */
-Data *createData(int val ) {
-  Data *new = malloc(sizeof(Data));
-  new->value = val;
-  return new;
+Data* createData(int val)
+{
+	Data* new=malloc(sizeof(Data));
+	new->value=val;
+	return new;
 }
 
 /*
  * allocate and initialise a List data structure 
  */
-List *createList( void ) {
-    List *new = malloc(sizeof(List));
-    new->blockSize = 10;
-    new->size = new->blockSize;
-    new->data = calloc(new->size,sizeof(Data *));  // allocate an initial block for List storage
-    new->length = 0;
+List* createList(void)
+{
+    List* new=malloc(sizeof(List));
+    new->blockSize=10;
+    new->size=new->blockSize;
+    new->data=calloc(new->size,sizeof(Data*));  // allocate an initial block for List storage
+    new->length=0;
     return new;
 }
 
 /*
  * reallocate dynamic array data if we have reached list->size
  */
-void enlargeList( List *list ) {
-    list->size += list->blockSize;
-    list->data = realloc(list->data,list->size); // allocate a further 'block' to the List to increase size
+void enlargeList(List* list)
+{
+    list->size+=list->blockSize;
+    list->data=realloc(list->data,list->size); // allocate a further 'block' to the List to increase size
     return;
 }
 
 /*
  * insert a Data item onto the List 
  */
-void insert( List *list, Data *new, int loc ) {
+void insert(List* list, Data* new, int loc)
+{
     
-    if( list->length+1 == list->size ) // reallocate if we have reached max size
-        enlargeList( list );
+    if(list->length+1==list->size) // reallocate if we have reached max size
+	{
+        enlargeList(list);
+	}
 
-    if( loc>list->length || loc<0 ) // check for invalid loc
+    if(loc>list->length || loc<0) // check for invalid loc
+	{
         return;
+	}
 
     // shuffle list up to create a gap at index loc
-    for( int k=list->length; k>loc; --k )
-        list->data[k] = list->data[k-1];
+    for(int k=list->length; k>loc; --k)
+	{
+        list->data[k]=list->data[k-1];
+	}
 
-    list->data[loc] = new;   // insert the data at loc
+    list->data[loc]=new;   // insert the data at loc
     list->length++;          // increase the list length
 
     return;
@@ -59,8 +69,9 @@ void insert( List *list, Data *new, int loc ) {
 /*
  * remove the front Data item from the List
  */
-Data *delete( List *list, int loc ) {
-    Data *new = NULL;
+Data* delete(List* list, int loc)
+{
+    Data* new=NULL;
     
     // check for empty List
 
@@ -78,9 +89,9 @@ Data *delete( List *list, int loc ) {
 /*
  * display List data - traverse list from front to back
  */
-void displayList ( List *list ) {
+void displayList (List* list) {
     printf("List length %d\n",list->length);
-    for( int k=0; k<list->length; ++k )
+    for(int k=0; k<list->length; ++k)
         printf(" %d",list->data[k]->value);
     printf("\n");
     return;
@@ -89,9 +100,13 @@ void displayList ( List *list ) {
 /*
  * free dynamic array data
  */
-void freeList( List *list ) {
-    for( int k=0; k<list->size; ++k )
-        free( list->data[k] ); // free List Data item
-    free( list->data );        // free List Data array
+void freeList(List* list)
+{
+    for(int k=0; k<list->size; ++k)
+	{
+		free(list->data[k]); // free List Data item
+	}
+    free(list->data);        // free List Data array
+    free(list);                // free List
     return;
 }
--- session-2/task2/main.c
+++ session-2/task2/main.c
@@ -5,19 +5,19 @@
 #include "list_structures.h"
 #include "list.h"
 
-int main( void ) {
+int main(void)
+{
 
-    List *list = createList();
+    List* list=createList();
 
     // insert() - add some data to the list
-    insert( list, createData(1), 0 ); // add with value 1 at index 0
-    displayList( list );
+    insert(list, createData(1), 0); // add with value 1 at index 0
+    displayList(list);
 
     // further insert operations here, use displayList() to verify
 
     // free list dynamic memory
-    freeList( list );
-    free( list );
+    freeList(list);
     
     return 0;
 }
